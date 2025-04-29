17d0bc2287a592f7b77b70705a8fe6c424de837d
Ömer Faruk Erzurumluoğlu <omer.erzurumluoglu@std.bogazici.edu.tr>
2025-04-30 01:18:57 +0300

    Small changes

--- apps/zenoh_bridge/test_sub.cpp
+++ apps/zenoh_bridge/test_sub.cpp
@@ -29,7 +29,7 @@ int main(int argc, char* argv[])
 {
     zenoh::Config sub_config_def = zenoh::Config::create_default();
     sub_config_def.insert_json5("connect/endpoints", R"(["tcp/127.0.0.1:7447"])");
-    sub_config_def.insert_json5("listen/endpoints", R"(["tcp/0.0.0.0:7447"])");
+    sub_config_def.insert_json5("listen/endpoints", R"(["tcp/0.0.0.0:7448"])");
     auto sub_session = zenoh::Session::open(std::move(sub_config_def));
     std::cout << "Declaring Subscriber on 'output/**'..." << std::endl;
     auto subscriber = sub_session.declare_subscriber(
